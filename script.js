63909f64d232e4f3bf288b6f1b03a714b055bde8
Asmita Dhananjay Deshpande <asmitadeshpande@mac.mynetworksettings.com>
2024-12-29 16:48:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+});
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
